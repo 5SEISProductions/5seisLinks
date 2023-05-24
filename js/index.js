44d358f28b470affea67f7bfbfb7aebfd3583e65
Ivan Toneatto <ivan-ariel@hotmail.com>
2023-05-24 15:44:31 -0300

    build 1

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function gotodisegrafico(num){
+    if(num == 1){
+        document.getElementById('disegrafico').click()
+    }
+    if(num == 2){
+        document.getElementById('liderproyecto').click()
+    }
+    if(num == 3){
+        document.getElementById('produccion').click()
+    }
+    if(num == 4){
+        document.getElementById('animacion').click()
+    }
+    if(num == 5){
+        document.getElementById('programacion').click()
+    }
+    if(num == 6){
+        document.getElementById('website').click()
+    }
+}
+
